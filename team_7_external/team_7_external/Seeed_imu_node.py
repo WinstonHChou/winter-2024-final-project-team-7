6cd73ad8a5244c5cc935c22598c8854a213d9b7a
WinstonHChou <wisnton.h.chou@gmail.com>
2024-03-15 01:42:24 +0000

    added razorIMU9dof_submodule

--- team_7_external/team_7_external/Seeed_imu_node.py
+++ team_7_external/team_7_external/Seeed_imu_node.py
@@ -6,7 +6,15 @@ import yaml
 import threading
 
 config_path = "/home/projects/ros2_ws/src/razorIMU_9dof/razor.yaml"
-imu = RazorIMU([config_path])
-imu.start()
-time.sleep(2)
-print(imu.orient[])
+
+if __name__ == "__main__":
+    razor_imu = RazorIMU('razor.yaml')
+    razor_imu.start()
+    time.sleep(2)
+    for t in range(1,20):
+        print("X: " + str(razor_imu.orient[AxisRef.X.value]))
+        print("Y: " + str(razor_imu.orient[AxisRef.Y.value]))
+        print("Z: " + str(razor_imu.orient[AxisRef.Z.value]))
+        time.sleep(1)
+    razor_imu.shutdown()
+    print("All Good!")
